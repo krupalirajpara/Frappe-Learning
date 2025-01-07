1b1eae3dd5cbb1fa42c3fa88fe11b45b75589852
parth136 <parth@sanskartechnolab.com>
2025-01-07 17:19:29 +0530

    first-commit

---
+++ librarymanagement/librarymanagement/doctype/library_membership/library_membership.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2025, Demo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Library Membership", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+frappe.ui.form.on('Library Membership', {
+    refresh: function(frm) {
+        frm.add_custom_button('Create Transaction', () => {
+        frappe.new_doc('Library Transaction', {
+        library_member: frm.doc.name
+        })
+        })
+    }
+   });
+   
