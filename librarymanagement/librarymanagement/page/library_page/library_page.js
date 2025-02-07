3b3e1374465b9963a1fee30e379d7bd2e5494a7c
krupalirajpara <krupali@sanskartechnolab.com>
2025-02-07 19:12:19 +0530

    learning

---
+++ librarymanagement/librarymanagement/page/library_page/library_page.js
@@ -0,0 +1,47 @@
+frappe.pages['library-page'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Library Management System',
+		single_column: true
+	});
+
+
+	
+	page.set_title('Library Management System (For Learning)')
+	page.set_indicator('Process', 'red')
+	let $primary_btn = page.set_primary_action('New', ()=> frappe.msgprint('Button Clicked!'), 'octicon octicon-plus')
+	let $secondary_btn = page.set_secondary_action('Refresh', ()=> frappe.msgprint('Button Clicked!'), 'octicon octicon-plus')
+	page.add_menu_item('Send Email', ()=> frappe.msgprint('Send Mail Button Clicked!'))
+	page.add_action_item('Delete', ()=> frappe.msgprint('Delete Button Clicked!'))
+
+	let first_name = page.add_field({
+		label: 'First Name', 
+		fieldtype: 'Data',
+		fieldname: 'first_name',
+	});
+	let last_name = page.add_field({
+		label: 'Last Name', 
+		fieldtype: 'Data',
+		fieldname: 'last_name',
+	});
+	let select_gender = page.add_field({
+		label: 'Gender', 
+		fieldtype: 'Select',
+		fieldname: 'gender',
+		options: ['Male', 'Female', 'Other'],
+		change(){
+			frappe.msgprint(field.get_value());
+		}
+	});
+	let library_name = page.add_field({
+		label: 'Choose Library', 
+		fieldtype: 'Link',
+		fieldname: 'library_name',
+		options: 'Library'
+	});
+	// $(frappe.render_template("library_page", {})).appendTo(page.body);
+
+	$(frappe.render_template("library_page", {
+		data: " Hello "
+	})).appendTo(page.body);
+}
---
+++ librarymanagement/librarymanagement/report/library_script_report/library_script_report.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2025, Demo and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Library Script Report"] = {
+	"filters": [
+		{
+			'fieldname': 'library_name',
+			'label': 'Library',
+			'fieldtype': 'Data',
+			// 'options': '',
+		},
+
+		// {
+		// 	'fieldname': 'article',
+		// 	'label': 'Article',
+		// 	'fieldtype': 'Table',
+		// 	'options': 'article',
+		// }
+	]
+};
